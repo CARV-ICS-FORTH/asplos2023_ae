90d21e25759edf8b0df4cd2495d9949c3c02d3ee
Jack Kolokasis <kolokasis@ics.forth.gr>
2023-01-26 15:16:21 +0200

    [fix] Rename scripts

---
+++ scripts/experiments/figure5/gen_fig5.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+###################################################
+#
+# file: gen_fig5.sh
+#
+# @Author:   Iacovos G. Kolokasis
+# @Version:  24-01-2023 
+# @email:    kolokasis@ics.forth.gr
+#
+# Generate all the plots for Figure5
+#
+###################################################
+
+./spark_pr.sh
+./spark_cc.sh
+./spark_sssp.sh
+./spark_svd.sh
+./spark_tr.sh
+./spark_lr.sh
+./spark_lgr.sh
+./spark_svm.sh
+./spark_bc.sh
+./spark_sql.sh
